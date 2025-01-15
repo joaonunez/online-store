7b1ec6e70bb37b7e960ac7dd4e6d55a7da46588e
joaonunez <joaovaldiglesias@gmail.com>
2025-01-15 16:17:01 -0300

    ejercicio finalizado

--- OnlineStore.java
+++ OnlineStore.java
@@ -9,8 +9,8 @@ public class OnlineStore {
         // products:
         double bookPrice = 15.99;
         double tShirtPrice = 25.50;
-        double pantsPrice = 40.00;
-        double shoesPrice = 50.00;
+        double pantsPrice = 100.00;
+        double shoesPrice = 30.00;
 
         // customers:
         String customer1 = "Ana";
@@ -46,7 +46,7 @@ public class OnlineStore {
         System.out.println(welcomeMessage + customer2);
         if (confirmOrderCustomer2) {
             System.out.println(customer2 + confirmationMessage);
-            System.out.println(showTotalMessage + (bookPrice + shoesPrice));
+            System.out.println(showTotalMessage + (tShirtPrice + shoesPrice + pantsPrice));
         }
         System.out.println(borderInner);
         System.out.println(border);
@@ -60,6 +60,26 @@ public class OnlineStore {
             System.out.println(customer3 + confirmationMessage);
             System.out.println(showTotalMessage + (tShirtPrice + pantsPrice));
         }
+        System.out.println(borderInner);
+        System.out.println(border);
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+         // Error en el pedido de Miguel
+        System.out.println("\n" + border);
+        System.out.println("         ERROR EN EL PEDIDO DE MIGUEL    ");
+        System.out.println(borderInner);
+        System.out.println("Lo sentimos, " + customer3 + ", ha habido un error en tu compra");
+        System.out.println("Te cobraron pantalones y una camiseta, pero solo deberías haber pagado por unos zapatos y un libro.");
+
+        double totalIncorrectAmountMiguel = (pantsPrice + tShirtPrice);
+        double totalCorrectAmountMiguel = (shoesPrice + bookPrice);
+        double diferenceAmountsOrderMiguel = (totalIncorrectAmountMiguel - totalCorrectAmountMiguel);
+        System.out.println("El total incorrecto fue: " + totalIncorrectAmountMiguel);
+        System.out.println("El Total correcto es: " + totalCorrectAmountMiguel);
+        System.out.println("La diferencia es: " + diferenceAmountsOrderMiguel);
         System.out.println(borderInner);
         System.out.println(border);
 
